637adcbe1b2fd3c8d27f46e20445f123c03fe5a4
MinasMazar <minasmazar@gmail.com>
2015-07-10 20:44:36 +0200

    Add PRY gem for debugging purposes.

--- test/test_i3rb.rb
+++ test/test_i3rb.rb
@@ -1,9 +1,10 @@
 require 'minitest_helper'
+require 'pry'
 
 class TestI3rb < Minitest::Test
 
   @@api_driver = Object.new.extend I3::API
-  @@i3bar = Object.new.extend I3::Bar
+  @@i3bar = I3::Bar.get_instance
 
   def test_that_it_has_a_version_number
     refute_nil ::I3rb::VERSION
@@ -14,6 +15,9 @@ class TestI3rb < Minitest::Test
   end
 
   def test_I3BAR_launch_bar
+    @@i3bar.add_widget I3::Bar::BasicWidgets::Hostname
+    @@i3bar.run_widgets
+    sleep 0.8
     @@i3bar.to_stdout
   end
 
