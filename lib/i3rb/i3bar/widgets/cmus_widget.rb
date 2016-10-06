96198089ffcd7c367d47fad2f1af8c94e4377aca
MinasMazar <minasmazar@gmail.com>
2016-10-06 14:49:41 +0200

    Improve CMUS widget visualization

--- lib/i3rb/i3bar/widgets/cmus_widget.rb
+++ lib/i3rb/i3bar/widgets/cmus_widget.rb
@@ -9,39 +9,39 @@ module I3
         end
         def initialize(timeout)
           super :cmus, timeout do |w|
-	    cmus = `cmus-remote --query`
-	    if md = cmus.match(/status\s(\w+)/)
-	      status = md[1]
-	    end
-	    if md = cmus.match(/tag title\s(.+)/)
-	      title = md[1]
-	    end
-	    if md = cmus.match(/tag artist\s(.+)/)
-	      artist = md[1]
-	    end
-	    if title.nil? || artist.nil? || title.empty? || artist.empty?
-	      track_desc = "No Tags :("
-	    else
-	      track_desc = "#{title} - #{artist}"
-	    end
-	    if status == "playing"
-	      "🎜 #{track_desc} 🎜"
-	    elsif status == "paused"
-	      "🎜 #{track_desc} (pause) 🎜"
-	    elsif status == "stopped"
-	      "🎜 No Playing 🎜"
-	    else
-	      w.timeout = 60
-	      "🎜 CMUS inactive 🎜"
-	    end
+	          cmus = `cmus-remote --query`
+	          if md = cmus.match(/status\s(\w+)/)
+	            status = md[1]
+	          end
+	          if md = cmus.match(/tag title\s(.+)/)
+	            title = md[1]
+	          end
+	          if md = cmus.match(/tag artist\s(.+)/)
+	            artist = md[1]
+	          end
+	          if title.nil? || artist.nil? || title.empty? || artist.empty?
+	            track_desc = "No Tags :("
+	          else
+	            track_desc = "#{title} - #{artist}"
+	          end
+	          if status == "playing"
+	            "#{track_desc} |>"
+	          elsif status == "paused"
+	            "#{track_desc} ||"
+	          elsif status == "stopped"
+	            "No Playing"
+	          else
+	            w.timeout = 60
+	            "CMUS inactive"
+	          end
           end
 
           add_event_callback do |w,e|
-	    if e.button == 1
-	      system "cmus-remote", "--pause"
-	    elsif e.button == 3
-	      system "cmus-remote", "--next"
-	    end
+	          if e.button == 1
+	            system "cmus-remote", "--pause"
+	          elsif e.button == 3
+	            system "cmus-remote", "--next"
+	          end
           end
         end
       end
